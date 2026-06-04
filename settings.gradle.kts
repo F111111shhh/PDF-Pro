b9922ac7a180928b7bf55a7229fbdefa38fbd42b
F111111shhh <1936411577@qq.com>
2026-06-04 22:38:43 +0800

    Use official Maven repositories first

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,23 @@
 pluginManagement {
     repositories {
-        // 1. 添加阿里云镜像源（放在最前面）
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-
-        // 2. 原有的源保留在后面作为备用
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // 1. 添加阿里云镜像源
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-
-        // 2. 原有的源
         google()
         mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
 
-// 确保这里的名字和你项目名一致，根据你截图看是 PDFpro
 rootProject.name = "PDFpro"
 include(":app")
